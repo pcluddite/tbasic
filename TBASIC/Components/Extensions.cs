df897e2f9c92e7e657bb8cb6f893e3771a684d7a
Tim Baxendale <pcluddite@hotmail.com>
2016-02-08 04:27:08 -0600

    Added EqualsIgnoreCase to unify case insensitive equalities

---
+++ TBASIC/Components/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tbasic
+{
+    internal static class Extensions
+    {
+        public static bool EqualsIgnoreCase(this string initial, string other)
+        {
+            return initial.Equals(other, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
