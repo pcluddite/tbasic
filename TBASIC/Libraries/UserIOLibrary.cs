ae27dc114590127de82543156febce328f336129
Tim Baxendale <pcluddite@hotmail.com>
2016-05-21 22:14:39 -0500

    Exposed UserIOLibrary API

--- TBASIC/Libraries/UserIOLibrary.cs
+++ TBASIC/Libraries/UserIOLibrary.cs
@@ -26,8 +26,14 @@ using Tbasic.Errors;
 
 namespace Tbasic.Libraries
 {
-    internal class UserIOLibrary : Library
+    /// <summary>
+    /// A library for basic user input and output operations
+    /// </summary>
+    public class UserIOLibrary : Library
     {
+        /// <summary>
+        /// Initializes a new instance of this class
+        /// </summary>
         public UserIOLibrary()
         {
             Add("TrayTip", TrayTip);
@@ -78,6 +84,20 @@ namespace Tbasic.Libraries
             _sframe.Data = Console.ReadKey(true).KeyChar;
         }
 
+        /// <summary>
+        /// Prompts the user to input data
+        /// </summary>
+        /// <param name="prompt">the text for the message prompt</param>
+        /// <param name="title">the title of the message box</param>
+        /// <param name="defaultResponse">the default response</param>
+        /// <param name="x">the x position of the window</param>
+        /// <param name="y">the y position of the window</param>
+        /// <returns></returns>
+        public static string InputBox(string prompt, string title = "", string defaultResponse = "", int x = -1, int y = -1)
+        {
+            return Interaction.InputBox(prompt, title, defaultResponse, x, y);
+        }
+
         private void Input(StackFrame _sframe)
         {
             if (_sframe.Count == 2) {
@@ -101,18 +121,27 @@ namespace Tbasic.Libraries
             int x = _sframe.Get<int>(3),
                 y = _sframe.Get<int>(4);
 
-            string resp = Interaction.InputBox(
-                _sframe.Get<string>(1),
-                _sframe.Get<string>(2),
-                "", x, y);
+            string resp = InputBox(_sframe.Get<string>(1), _sframe.Get<string>(2), "", x, y);
 
-
-            if (resp == null || resp.Equals("")) {
+            if (string.IsNullOrEmpty(resp)) { 
                 _sframe.Status = ErrorSuccess.NoContent; // -1 no input 2/24
             }
             else {
                 _sframe.Data = resp;
             }
+        }
+
+        /// <summary>
+        /// Creates a notification balloon
+        /// </summary>
+        /// <param name="text">the text of the balloon</param>
+        /// <param name="title">the title of the balloon</param>
+        /// <param name="icon">the balloon icon 0 = none, 1 = info, 2 = warn, 3 = error </param>
+        /// <param name="timeout">the length of time the balloon should be shown (this may not be honored by the OS)</param>
+        public static void TrayTip(string text, string title = "", int icon = 0, int timeout = 5000)
+        {
+            Thread t = new Thread(MakeTrayTip);
+            t.Start(new object[] { timeout, icon, text, title });
         }
 
         private void TrayTip(StackFrame _sframe)
@@ -130,15 +159,10 @@ namespace Tbasic.Libraries
                 _sframe.Add(5000); // timeout
             }
             _sframe.AssertArgs(5);
-            string text = _sframe.Get<string>(1);
-            string title = _sframe.Get<string>(2);
-            int icon = _sframe.GetIntRange(3, 0, 3);
-            int timeout = _sframe.Get<int>(4);
-            Thread t = new Thread(TrayTip);
-            t.Start(new object[] { timeout, icon, text, title });
+            TrayTip(text: _sframe.Get<string>(1), title: _sframe.Get<string>(2), icon: _sframe.GetIntRange(3, 0, 3), timeout: _sframe.Get<int>(4));
         }
 
-        private void TrayTip(object param)
+        private static void MakeTrayTip(object param)
         {
             try {
                 object[] cmd = (object[])param;
@@ -154,7 +178,7 @@ namespace Tbasic.Libraries
                         default: icon = ToolTipIcon.None; break;
                     }
                     tray.ShowBalloonTip(timeout, (string)cmd[3], (string)cmd[2], icon);
-                    System.Threading.Thread.Sleep(timeout);
+                    Thread.Sleep(timeout);
                     tray.Visible = false;
                 }
             }
@@ -162,7 +186,19 @@ namespace Tbasic.Libraries
             }
         }
 
-        public static void MsgBox(StackFrame _sframe)
+        /// <summary>
+        /// Creates a message box
+        /// </summary>
+        /// <param name="prompt"></param>
+        /// <param name="buttons"></param>
+        /// <param name="title"></param>
+        /// <returns></returns>
+        public static string MsgBox(object prompt, int buttons = (int)MsgBoxStyle.ApplicationModal, object title = null)
+        {
+            return Interaction.MsgBox(prompt, (MsgBoxStyle)buttons, title).ToString();
+        }
+
+        private void MsgBox(StackFrame _sframe)
         {
             if (_sframe.Count == 3) {
                 _sframe.Add("");
@@ -173,17 +209,26 @@ namespace Tbasic.Libraries
             string text = _sframe.Get<string>(2),
                    title = _sframe.Get<string>(3);
 
-            _sframe.Data = Interaction.MsgBox(text, (MsgBoxStyle)flag, title).ToString();
+            _sframe.Data = MsgBox(buttons: flag, prompt: text, title: title);
+        }
+
+        /// <summary>
+        /// Converts text to synthesized speech
+        /// </summary>
+        /// <param name="text">the text to speak</param>
+        public static void Say(string text)
+        {
+            Thread t = new Thread(Say);
+            t.Start(text);
         }
 
         private void Say(StackFrame _sframe)
         {
             _sframe.AssertArgs(2);
-            Thread t = new Thread(Say);
-            t.Start(_sframe.Get<string>(1));
+            Say(_sframe.Get<string>(1));
         }
 
-        public void Say(object text)
+        private static void Say(object text)
         {
             try {
                 using (SpeechSynthesizer ss = new SpeechSynthesizer()) {
