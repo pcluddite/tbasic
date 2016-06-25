844082297803babf9c996ef9facff6049105a42f
Tim Baxendale <pcluddite@hotmail.com>
2016-06-25 15:45:13 -0500

    Moved Parsing to Runtime/Parsing

---
+++ TBASIC/Runtime/Parsing/DefinedRegex.cs
@@ -0,0 +1,99 @@
+﻿/**
+ *  TBASIC
+ *  Copyright (C) 2013-2016 Timothy Baxendale
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ **/
+using System.Text.RegularExpressions;
+
+namespace Tbasic.Runtime
+{
+    /// <summary>
+    /// This class is internal to the library, houses different regular expression objects
+    /// </summary>
+    internal class DefinedRegex
+    {
+        private const string c_strNumeric       = @"(?:[0-9]+)?(?:\.[0-9]+)?(?:E-?[0-9]+)?(?=\b)";
+        private const string c_strHex           = @"0x([0-9a-fA-F]+)";
+        private const string c_strBool          = @"true|false";
+        private const string c_strFunction      = @"([a-zA-Z][a-zA-Z0-9]*)\s*\((.*)\)";
+        private const string c_strVariable      = @"(([a-zA-Z_][a-zA-Z0-9_]*)\$|\@([a-zA-Z_][a-zA-Z0-9_]*))(\s*\[(.*)\])?";
+        private const string c_strString        = @"\""((\\"")|[^""])*\""|\'((\\')|[^'])*\'";
+        private const string c_strNull          = @"null";
+
+        private const string c_strUnaryOp       = @"(?:\+|-|NOT |~)(?=\w|\()";
+        private const string c_strBinaryOp      = @"<<|>>|\+|-|\*|/|MOD|AND|OR|&|\||\^|==|!=|<>|>=|=>|<=|=<|=|<|>";
+        private const string c_strWhiteSpace    = @"\s+";
+
+        internal static Regex Numeric = new Regex(
+            c_strNumeric,
+            RegexOptions.Compiled
+        );
+
+        internal static Regex Hexadecimal = new Regex(
+            c_strHex,
+            RegexOptions.Compiled
+        );
+
+        internal static Regex Boolean = new Regex(
+            c_strBool,
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        internal static Regex UnaryOp = new Regex(
+            @"(?<=(?:" + c_strBinaryOp + @")\s*|\A)(?:" + c_strUnaryOp + @")",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        internal static Regex BinaryOp = new Regex(
+            @"(?<!(?:" + c_strBinaryOp + @")\s*|^\A)(?:" + c_strBinaryOp + @")",
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        internal static Regex Parenthesis = new Regex(
+            @"\(",
+            RegexOptions.Compiled
+        );
+
+        internal static Regex Function = new Regex(
+            c_strFunction,
+            RegexOptions.Compiled
+        );
+
+        internal static Regex Variable = new Regex(
+            c_strVariable,
+            RegexOptions.Compiled
+        );
+
+        internal static Regex String = new Regex(
+            c_strString,
+            RegexOptions.Compiled
+        );
+
+        internal static Regex Null = new Regex(
+            c_strNull,
+            RegexOptions.Compiled | RegexOptions.IgnoreCase
+        );
+
+        internal static Regex WhiteSpace = new Regex(
+            c_strWhiteSpace,
+            RegexOptions.Compiled
+        );
+
+        static DefinedRegex() { }
+
+    }
+}
---
+++ TBASIC/Runtime/Parsing/Line.cs
@@ -0,0 +1,167 @@
+﻿/**
+ *  TBASIC
+ *  Copyright (C) 2013-2016 Timothy Baxendale
+ *  
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ *  USA
+ **/
+using System;
+
+namespace Tbasic
+{
+    /// <summary>
+    /// Defines a set of methods and properties for a line of Tbasic code
+    /// </summary>
+    public class Line : IComparable<Line>, IEquatable<Line>
+    {
+        /// <summary>
+        /// Initializes a line of Tbasic code
+        /// </summary>
+        /// <param name="id">The id of the line. This should be the line number.</param>
+        /// <param name="line">The text of the line</param>
+        public Line(int id, string line)
+        {
+            LineNumber = id;
+            Text = line.Trim(); // Ignore leading and trailing whitespace.
+            VisibleName = Name;
+        }
+
+        /// <summary>
+        /// Initializes a line of Tbasic code carring the same information as another Tbasic.Line
+        /// </summary>
+        /// <param name="line"></param>
+        public Line(Line line)
+        {
+            LineNumber = line.LineNumber;
+            Text = line.Text;
+            VisibleName = line.Name;
+        }
+
+        /// <summary>
+        /// Gets or sets the line number
+        /// </summary>
+        public int LineNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of this line
+        /// </summary>
+        public virtual string Text { get; set; }
+
+        /// <summary>
+        /// Gets or sets the block in which this line is nested
+        /// </summary>
+        public CodeBlock CurrentBlock { get; set; }
+
+        private string visibleName = null;
+
+        /// <summary>
+        /// Gets or sets the name of the line displayed in exceptions
+        /// </summary>
+        public string VisibleName
+        {
+            get {
+                if (visibleName == null) {
+                    return Name;
+                }
+                return visibleName;
+            }
+            set {
+                visibleName = value;
+            }
+        }
+
+        private string name = null;
+
+        /// <summary>
+        /// Retrieves the name that is retreived from the Tbasic.ObjectContext libraries
+        /// </summary>
+        public string Name
+        {
+            get {
+                if (name == null) { // This way we don't have to do this every time
+                    int bracket = Text.IndexOf('(');
+                    int space = Text.IndexOf(' ');
+                    if (bracket < 0 && space < 0) {
+                        name = Text;
+                    }
+                    else if (bracket < 0 && space > 0) {
+                        name = Text.Remove(space);
+                    }
+                    else if (space < 0 && bracket > 0) {
+                        name = Text.Remove(bracket);
+                    }
+                    else if (space < bracket) {
+                        name = Text.Remove(space);
+                    }
+                    else {
+                        name = Text.Remove(bracket);
+                    }
+                }
+                return name;
+            }
+        }
+
+        /// <summary>
+        /// Returns the text that this line represents
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Text;
+        }
+
+        /// <summary>
+        /// Compares this Tbasic.Line to another Tbasic.Line by comparing their LineNumber
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(Line other)
+        {
+            return LineNumber.CompareTo(other.LineNumber);
+        }
+
+        /// <summary>
+        /// Determines whether the specified System.Object is equal to the current System.Object
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public override bool Equals(object other)
+        {
+            if (other is Line) {
+                return this.Equals((Line)other);
+            }
+            return base.Equals(other);
+        }
+
+        /// <summary>
+        /// Hash code for the LineNumber
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return LineNumber.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determines if two Tbasic.Line objects share the same LineNumber
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Line other)
+        {
+            return other.LineNumber == LineNumber;
+        }
+    }
+}
